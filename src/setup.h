fded13bfdcaef3dbcb00ecf77816b3897fd7c369
Timothy Ellsworth Bowers <tbowers7@gmail.com>
2021-01-23 15:00:54 -0700

    Update copyright year
    	modified:   src/demo.c
    	modified:   src/demo.h
    	modified:   src/display.c
    	modified:   src/display.h
    	modified:   src/fitsw.c
    	modified:   src/fitsw.h
    	modified:   src/images.c
    	modified:   src/images.h
    	modified:   src/init.c
    	modified:   src/init.h
    	modified:   src/main.c
    	modified:   src/mirrors.c
    	modified:   src/mirrors.h
    	modified:   src/rays.c
    	modified:   src/rays.h
    	modified:   src/sd_defs.h
    	modified:   src/setup.c
    	modified:   src/setup.h
    	modified:   src/ui.c
    	modified:   src/ui.h
    	modified:   src/vectors.c
    	modified:   src/vectors.h

--- src/setup.h
+++ src/setup.h
@@ -5,7 +5,7 @@
  * 
  * FILE: setup.h
  * 
- * Copyright (C) 2016-2020  Timothy P. Ellsworth Bowers
+ * Copyright (C) 2016-2021  Timothy P. Ellsworth Bowers
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
